610e1edea05e7260c94de510e3dd3e81c899851f
Gautam C <gchitnis@scu.edu>
2022-11-17 20:36:13 -0800

    Added FIH About Us Page

--- app/src/main/java/com/scu/lifepassage/AboutUsActivity.java
+++ app/src/main/java/com/scu/lifepassage/AboutUsActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class AboutUsActivity extends AppCompatActivity {
 
@@ -11,6 +14,12 @@ public class AboutUsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
-        Intent intent = getIntent();
+        final ImageButton button = findViewById(R.id.fih);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(AboutUsActivity.this, FihActivity.class);
+                AboutUsActivity.this.startActivity(myIntent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/scu/lifepassage/FihActivity.java
@@ -0,0 +1,15 @@
+package com.scu.lifepassage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class FihActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fih);
+    }
+}
