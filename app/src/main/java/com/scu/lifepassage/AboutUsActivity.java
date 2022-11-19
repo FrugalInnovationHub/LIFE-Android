effe16afd7dc921b7bd2348d7c461db23c228313
Gautam C <gchitnis@scu.edu>
2022-11-18 19:07:40 -0800

    Added About Us Pages
    - SCU Info
    - Passage Info
    - Students Info

--- app/src/main/java/com/scu/lifepassage/AboutUsActivity.java
+++ app/src/main/java/com/scu/lifepassage/AboutUsActivity.java
@@ -14,10 +14,36 @@ public class AboutUsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
-        final ImageButton button = findViewById(R.id.fih);
-        button.setOnClickListener(new View.OnClickListener() {
+
+        final ImageButton fih_button = findViewById(R.id.fih);
+        final ImageButton scu_button = findViewById(R.id.scu);
+        final ImageButton passage_button = findViewById(R.id.passage);
+        final ImageButton students_button = findViewById(R.id.students);
+
+        fih_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent myIntent = new Intent(AboutUsActivity.this, FihActivity.class);
+                AboutUsActivity.this.startActivity(myIntent);
+            }
+        });
+
+        scu_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(AboutUsActivity.this, ScuActivity.class);
+                AboutUsActivity.this.startActivity(myIntent);
+            }
+        });
+
+        passage_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(AboutUsActivity.this, PassageActivity.class);
+                AboutUsActivity.this.startActivity(myIntent);
+            }
+        });
+
+        students_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(AboutUsActivity.this, AboutStudentsActivity.class);
                 AboutUsActivity.this.startActivity(myIntent);
             }
         });
